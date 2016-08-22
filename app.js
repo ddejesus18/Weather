806ad36336fb543f8489eeb1a92887a54f8c229f
Diana De Jesus <diana@hotjar.com>
2016-08-21 21:12:23 -0400

    started localhost, need to connect lat and lon

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyB83AKi087BJ9AxX1j40Bg5I8GSp8i6hVw", false);
-xhr.send();
+var options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0
+};
 
-var apiKey = "AIzaSyB83AKi087BJ9AxX1j40Bg5I8GSp8i6hVw";
-console.log(xhr.status);
-console.log(xhr.statusText);
-//get geolocation and add the lat and lon directly to the url. Is this possible?
+function success(pos) {
+  var crd = pos.coords;
+  console.log(pos.coords);
 
- window.onload = function() {
-   var startPos;
-   var geoSuccess = function(position) {
-     startPos = position;
-     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
-     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
-   };
-   navigator.geolocation.getCurrentPosition(geoSuccess);
- };
+};
+function getLat(pos) {
+  var crd = pos.coords;
+  return crd.latitude;
+}
+function getLon(pos) {
+  var crd = pos.coords;
+  return crd.longitude;
+}
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+function loadDoc() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystate = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log("it's working");
+    }
+  };
+  xhr.open('GET', "api.openweathermap.org/data/2.5/weather?lat={Coordinates.latitude}&lon={Coordinates.longitude}&APPID=88b762769b10154d4b26076e5c47c8ec", true);
+  xhr.send();
+}
+//need to update so that request is after the page has loaded also confirm whether or not
+//we are accessing lat and lon
